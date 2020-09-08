869355c898c82ca1f8ce55d619c70c874634ac29
Adnan Salahuddin <adnan.salah84@gmail.com>
2020-09-08 10:46:01 +0500

    create connection and add primeng package

---
+++ InvoiceManagementSystem/InvoiceManagementSystem/DatabaseContext/InvoiceManagementDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementSystem.DatabaseContext
+{
+    public class InvoiceManagementDbContext : DbContext
+    {
+        public InvoiceManagementDbContext(DbContextOptions<InvoiceManagementDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- InvoiceManagementSystem/InvoiceManagementSystem/Startup.cs
+++ InvoiceManagementSystem/InvoiceManagementSystem/Startup.cs
@@ -1,6 +1,8 @@
+using InvoiceManagementSystem.DatabaseContext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +21,10 @@ namespace InvoiceManagementSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<InvoiceManagementDbContext>(options =>
+             options.UseSqlServer(
+                 Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -33,6 +39,7 @@ namespace InvoiceManagementSystem
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }
             else
             {
